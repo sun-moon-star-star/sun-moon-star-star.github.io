83580cb41c210ba22e3de7accc2d0d641c5233e3
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-05-09 16:59:00 +0800

    feat(example): init

--- dist/js/simple.js
+++ dist/js/simple.js
@@ -1,22 +1,24 @@
-function downloadHTML() {
-    if (document.querySelector(".downHtml").getAttribute('href') == '#') {
-        var canvas2 = document.createElement("canvas");
-        let _canvas = document.querySelector('html');
-        document.querySelector(".downHtml").style.display = "none";
-        var w = parseInt(window.getComputedStyle(_canvas).width);
-        var h = parseInt(window.getComputedStyle(_canvas).height);
-        //将canvas画布放大若干倍，然后盛放在较小的容器内，就显得不模糊了
-        canvas2.width = w * 4;
-        canvas2.height = h * 4;
-        canvas2.style.width = w + "px";
-        canvas2.style.height = h + "px";
-        var context = canvas2.getContext("2d");
-        context.scale(4, 4);
+function setHTML() {
+    var canvas2 = document.createElement("canvas");
+    let _canvas = document.querySelector('html');
+    document.querySelector(".downHtml").style.display = "none";
+    var w = parseInt(window.getComputedStyle(_canvas).width);
+    var h = parseInt(window.getComputedStyle(_canvas).height);
+    //将canvas画布放大若干倍，然后盛放在较小的容器内，就显得不模糊了
+    canvas2.width = w * 4;
+    canvas2.height = h * 4;
+    canvas2.style.width = w + "px";
+    canvas2.style.height = h + "px";
+    var context = canvas2.getContext("2d");
+    context.scale(4, 4);
 
-        html2canvas(document.querySelector('html'), { canvas: canvas2 }).then(function (canvas) {
-            document.querySelector(".downHtml").setAttribute('href', canvas.toDataURL());
-            document.querySelector(".downHtml").style.display = "block";
-            document.querySelector(".downHtml").click();
-        });
-    }
+    html2canvas(document.querySelector('html'), { canvas: canvas2 }).then(function (canvas) {
+        document.querySelector(".downHtml").setAttribute('href', canvas.toDataURL());
+        document.querySelector(".downHtml").style.display = "block";
+        document.querySelector(".downHtml").click();
+    });
 }
+function downloadHTML() {
+    setHTML();
+}
+setHTML();
